9893f59be1f4ef5e1d03792e7bb57cc5d091615e
taha <muntahabms@gmail.com>
2026-05-27 01:00:10 +0600

    django internship project submission

--- taha_app/models.py
+++ taha_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Crop(models.Model):
+    crop_name = models.CharField(max_length=100)
+    planting_date = models.DateField()
+    watering_date = models.DateField()
+    fertilizer_date = models.DateField()
+    harvest_date = models.DateField()
+
+    def __str__(self):
+        return self.crop_name
--- taha_app/views.py
+++ taha_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {
+        "name": "Sidratul Muntaha",
+        "course": "Django Internship",
+        "fruits": ["Apple", "Banana", "Mango"]
+    }
+    return render(request, "home.html", context)
+
+def about(request):
+    context = {
+        "message": "This page demonstrates Django template inheritance."
+    }
+    return render(request, "about.html", context)
